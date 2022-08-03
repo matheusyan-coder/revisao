51271ed0d6dffbaa563b84d7bbaf4804c062ad5a
matheusyan-coder <84139856+matheusyan-coder@users.noreply.github.com>
2022-08-03 12:09:11 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -38,9 +38,17 @@ app.use(urlencoded({extended:false}))
 app.post('/',async(req,res)=>{
     const dados = req.body
     res.send(dados)
+    //res.send(dados)
+    const gravar = new infos({
+        nome:dados.nome,
+        turma:dados.turma,
+        disciplina:dados.disciplina
+    }).save()
+    res.redirect9('/')
 })
 
+const porta= process.env.PORT || 3050
 //ligar o servidor na porta 3050
-app.listen(3050, ()=>{
+app.listen(porta,()=>{
     console.log('servidor local em http://localhost:3050')
 })
